6c10fe1e9f913ea82b76d400f6ba1be125a5a84c
Sara Carreras <saracc621@gmail.com>
2022-11-16 12:04:10 +0100

    movieDetails usign store

--- src/features/movies/components/pages/moviedetails.tsx
+++ src/features/movies/components/pages/moviedetails.tsx
@@ -1,77 +1,58 @@
-import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import styles from "./moviedetails.module.scss";
-import { iMovie } from "../../interfaces/imovie";
+import { iParam } from "../../interfaces/imovie";
+import { RootState } from "../../../../infraestructure/store/store";
 
-// const movieInfo = "https://api.themoviedb.org/3/movie/550?api_key=04d110606a25e52db02f63a7d1e1d707";
-
-const API_KEY = "api_key=04d110606a25e52db02f63a7d1e1d707";
-const BASEAPI_URL = "https://api.themoviedb.org/3";
-const LANGUAGE = "&language=es&";
 const imageURL = "https://image.tmdb.org/t/p/w300/";
 
 function MovieDetails() {
-    const initialState: iMovie = {
-        title: "",
-        id: 0,
-        poster_path: "",
-        overview: "",
-        vote_average: 0,
-        genres: [{ id: 0, name: "" }],
-    };
-    const [movie, setMovie] = useState(initialState);
-    const { movieId } = useParams();
+    const movies = useSelector((state) => (state as RootState).movies);
 
-    // console.log("PARAM" + movieId);
-    const GetById = BASEAPI_URL + `/movie/${movieId}?` + API_KEY + LANGUAGE;
+    const { movieId } = useParams<keyof iParam>() as iParam;
 
-    useEffect(() => {
-        fetch(GetById)
-            .then((resp) => resp.json())
-            .then((data) => {
-                setMovie(() => ({
-                    title: data.title,
-                    id: data.id,
-                    poster_path: `${imageURL}` + data.poster_path,
-                    overview: data.overview,
-                    vote_average: data.vote_average,
-                    genres: data.genres,
-                }));
-                // console.log(data.results);
-            });
-    }, [GetById]);
+    const moviesFiletered = movies.filter(
+        (movie) => movie.id.toString() === movieId
+    );
 
-    if (movie && movie.genres != null) {
-        return (
-            <div className={styles.detailsContainer}>
-                <img
-                    className={styles.col}
-                    src={movie.poster_path}
-                    alt={movie.title}
-                />
-                <div className={styles.col}>
-                    <p>
-                        <strong>Título:</strong> {movie.title}
-                    </p>
+    return moviesFiletered ? (
+        <>
+            {moviesFiletered.map((element) => {
+                return element && element.id != null ? (
+                    <>
+                        <div
+                            key={element.id}
+                            className={styles.detailsContainer}
+                        >
+                            <img
+                                className={styles.col}
+                                src={`${imageURL}` + element.poster_path}
+                                alt={element.title}
+                            />
 
-                    <p>
-                        <strong>
-                            {movie.genres.map((genre) => genre.name).join(", ")}
-                        </strong>
-                    </p>
+                            <p>
+                                <strong>Description: </strong>
+                                {element.overview}
+                            </p>
+                        </div>
+                        <div className={styles.col}>
+                            <p>
+                                <strong>Título: </strong> {element.title}
+                            </p>
 
-                    <p>
-                        <strong>Description:</strong> {movie.overview}
-                    </p>
-                </div>
-            </div>
-        );
-    } else {
-        return (
-            <div>
-                <h1>There is an error!!!</h1>
-            </div>
-        );
-    }
+                            <p>
+                                <strong>Fecha de estreno: </strong>
+                                {element.release_date}
+                            </p>
+                        </div>
+                    </>
+                ) : (
+                    <h1>"We didn't find the Movie Details."</h1>
+                );
+            })}
+        </>
+    ) : (
+        <h1>"We didn't find the Movie."</h1>
+    );
 }
 export default MovieDetails;
