93a811f0857750e3170968958543abe39f918c0f
Sara Carreras <saracc621@gmail.com>
2022-11-17 18:50:45 +0100

    fixing footer floating

--- src/infraestructure/components/layout/layout/layout.tsx
+++ src/infraestructure/components/layout/layout/layout.tsx
@@ -1,14 +1,11 @@
-import { Fragment } from "react";
 import { Footer } from "../footer/footer";
 import MainNavigation from "../header/header";
-
-import styles from "./Layout.module.scss";
 
 const Layout = ({ children }: { children: JSX.Element }) => {
     return (
         <>
             <MainNavigation />
-            <main className={styles.main}>{children}</main>
+            <main>{children}</main>
             <Footer />
         </>
     );
