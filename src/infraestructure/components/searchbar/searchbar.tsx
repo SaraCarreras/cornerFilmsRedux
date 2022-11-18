5159fac0b1eff314eb835ac4c7cd0ad95728aa40
Sara Carreras <saracc621@gmail.com>
2022-11-18 18:27:26 +0100

    prevent default onsubmit

--- src/infraestructure/components/searchbar/searchbar.tsx
+++ src/infraestructure/components/searchbar/searchbar.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import styles from "./searchbar.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchBar() {
+    const handleSubmit = (e: SyntheticEvent) => {
+        e.preventDefault();
+    };
     return (
-        <form className={styles.searchContainer}>
+        <form className={styles.searchContainer} onSubmit={handleSubmit}>
             <div className={styles.searchBox}>
                 <input className={styles.searchInput} type={"text"}></input>
                 <button className={styles.searchButton} type="submit">
