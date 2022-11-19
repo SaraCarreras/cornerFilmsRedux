1ba54930d8ac0d41a882b52ea5a5d458f689e17c
Sara Carreras <saracc621@gmail.com>
2022-11-19 10:34:36 +0100

    search to query url with useState

--- src/infraestructure/components/searchbar/searchbar.tsx
+++ src/infraestructure/components/searchbar/searchbar.tsx
@@ -1,16 +1,25 @@
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, useState } from "react";
 import styles from "./searchbar.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar() {
+    const navigate = useNavigate();
+    const [searchText, setSearchText] = useState("");
     const handleSubmit = (e: SyntheticEvent) => {
         e.preventDefault();
+        navigate("/?search=" + searchText);
     };
     return (
         <form className={styles.searchContainer} onSubmit={handleSubmit}>
             <div className={styles.searchBox}>
-                <input className={styles.searchInput} type={"text"}></input>
+                <input
+                    className={styles.searchInput}
+                    type={"text"}
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                ></input>
                 <button className={styles.searchButton} type="submit">
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                 </button>
