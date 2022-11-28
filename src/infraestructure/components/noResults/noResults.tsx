76b6a4e2b587db2ee59bd186037b0d3d7c6081b5
Sara Carreras <saracc621@gmail.com>
2022-11-28 19:25:15 +0100

    creating empty results component

---
+++ src/infraestructure/components/noResults/noResults.tsx
@@ -0,0 +1,7 @@
+import styles from "./noResults.module.scss";
+
+export function NoResults() {
+    return (
+        <div className={styles.empty}>Sorry, there are no matching results</div>
+    );
+}
