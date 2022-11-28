de1e0fd08f3b3df51443e8e85a2ac4d3561a13cb
Sara Carreras <saracc621@gmail.com>
2022-11-28 19:44:08 +0100

    cleaning code

--- src/infraestructure/components/noResults/noResults.tsx
+++ src/infraestructure/components/noResults/noResults.tsx
@@ -2,8 +2,6 @@ import styles from "./noResults.module.scss";
 
 export function NoResults() {
     return (
-        <div className={styles.empty}>
-            Sorry, there are no matching results{" "}
-        </div>
+        <div className={styles.empty}>Sorry, there are no matching results</div>
     );
 }
